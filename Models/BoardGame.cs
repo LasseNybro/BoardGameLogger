3698a05b3f67bd0b44bf5e3a30c7acb02cf964f4
Lasse Nybro Nielsen <lasso290391@hotmail.com>
2019-11-12 12:46:58 +0100

    Add project files.

---
+++ Models/BoardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardGameLogger.Models
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public GenreType Genre { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+    }
+}
